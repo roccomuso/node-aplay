9371692f05535531c2f639b53dbdbead76eac327
Rocco <rocco@cloudplugs.com>
2017-02-14 12:10:31 +0100

    code standardized #1

--- node-aplay.js
+++ node-aplay.js
@@ -9,52 +9,53 @@
  * MIT License
  */
 
-var	spawn   = require('child_process').spawn,
-    exec    = require('child_process').exec,
-	  events  = require('events'),
-	  util    = require('util');
+var spawn = require('child_process').spawn
+var events = require('events')
+var util = require('util')
 
-module.exports = function Sound() {
-	events.EventEmitter.call(this);
-};
+function Sound () {
+  events.EventEmitter.call(this)
+}
 
-util.inherits(module.exports, events.EventEmitter);
+util.inherits(Sound, events.EventEmitter)
 
-module.exports.prototype.play = function (fileName) {
-	this.stopped = false;
-  if (typeof this.process !== 'undefined') this.process.kill('SIGTERM'); // avoid multiple play for the same istance
-	this.process = spawn('aplay', [ fileName ]);
-	var self = this;
-	this.process.on('exit', function (code, sig) {
-		if (code !== null && sig === null) {
-			self.emit('complete');
-		}
-	});
-};
-module.exports.prototype.stop = function () {
-  if (this.process){
-	   this.stopped = true;
-  	 this.process.kill('SIGTERM');
-	   this.emit('stop');
+Sound.prototype.play = function (fileName) {
+  this.stopped = false
+  if (typeof this.process !== 'undefined') this.process.kill('SIGTERM') // avoid multiple play for the same istance
+  this.process = spawn('aplay', [ fileName ])
+  var self = this
+  this.process.on('exit', function (code, sig) {
+    if (code !== null && sig === null) {
+      self.emit('complete')
+    }
+  })
+}
+Sound.prototype.stop = function () {
+  if (this.process) {
+    this.stopped = true
+    this.process.kill('SIGTERM')
+    this.emit('stop')
   }
-};
-module.exports.prototype.pause = function () {
-  if (this.process){
-    if (this.stopped) return;
-    this.process.kill('SIGSTOP');
-	  this.emit('pause');
+}
+Sound.prototype.pause = function () {
+  if (this.process) {
+    if (this.stopped) return true
+    this.process.kill('SIGSTOP')
+    this.emit('pause')
   }
-};
-module.exports.prototype.resume = function () {
-  if (this.process){
-	   if (this.stopped) return this.play();
-	   this.process.kill('SIGCONT');
-	   this.emit('resume');
+}
+Sound.prototype.resume = function () {
+  if (this.process) {
+    if (this.stopped) return this.play()
+    this.process.kill('SIGCONT')
+    this.emit('resume')
   }
-};
+}
+
+module.exports = Sound
 
 // autonomous execution: node node-aplay.js my-song.wav
-if(!module.parent) {
-    new module.exports()
-        .play(process.argv[2]);
+if (!module.parent) {
+  var player = new Sound()
+  player.play(process.argv[2])
 }
